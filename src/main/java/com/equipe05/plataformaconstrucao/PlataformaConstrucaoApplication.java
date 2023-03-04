2573dcab7f9d4446d97062513f996d577b15bed3
schelip <fcominscheffel@gmail.com>
2023-03-04 19:28:38 -0300

    T000.1: initial commit

---
+++ src/main/java/com/equipe05/plataformaconstrucao/PlataformaConstrucaoApplication.java
@@ -0,0 +1,13 @@
+package com.equipe05.plataformaconstrucao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaConstrucaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaConstrucaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/equipe05/plataformaconstrucao/PlataformaConstrucaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipe05.plataformaconstrucao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaConstrucaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
