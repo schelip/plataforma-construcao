5e18a9262447af1affd29781e746686312482763
Rcoky <rvinicius126@gmail.com>
2023-03-20 11:39:44 -0300

    T001.3: create UserController for add, remove and search for users that were created in the database through the repository

---
+++ src/main/java/com/equipe05/plataformaconstrucao/controller/UserController.java
@@ -0,0 +1,123 @@
+package com.equipe05.plataformaconstrucao.controller;
+
+
+import com.equipe05.plataformaconstrucao.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.equipe05.plataformaconstrucao.model.User;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@CrossOrigin(origins = "http://localhost:5432")
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user")
+    public ResponseEntity<List<User>> getAllUser(@RequestParam(required = false) String email) {
+        try {
+            List<User> user = new ArrayList<User>();
+
+            if (email == null)
+                userRepository.findAll().forEach(user::add);
+            else
+                userRepository.findByEmailContaining(email).forEach(user::add);
+            if (user.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        Optional<User> userData = userRepository.findById(id);
+
+        if (userData.isPresent()) {
+            return new ResponseEntity<>(userData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+       try {
+           User _user = userRepository.save(new User(user.getId(), user.getNickname(), user.getEmail(), user.getPassword(), user.getAge()));
+           return new ResponseEntity<>(_user, HttpStatus.CREATED);
+       } catch (Exception e) {
+           return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+       }
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        Optional<User> userData = userRepository.findById(id);
+
+        if (userData.isPresent()) {
+            User _user = userData.get();
+            _user.setEmail(user.getEmail());
+            _user.setNickname(user.getNickname());
+            _user.setAge(user.getAge());
+            _user.setPassword(user.getPassword());
+            return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") long id){
+        try {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/user")
+    public ResponseEntity<HttpStatus> deleteAllUser() {
+        try{
+            userRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/user/byNickname/{nickname}")
+    public ResponseEntity<List<User>> findByNickname(@PathVariable("nickname") String nickname) {
+        try {
+            List<User> users = userRepository.findByNickname(nickname);
+
+            if (users.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
