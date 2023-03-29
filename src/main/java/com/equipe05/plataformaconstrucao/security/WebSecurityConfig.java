f16c6926b740c691a3ac10bb1c6878e2dd81b7a8
schelip <fcominscheffel@gmail.com>
2023-03-29 11:27:58 -0300

    T001.4: disable CORS blocking and add current session getter

--- src/main/java/com/equipe05/plataformaconstrucao/security/WebSecurityConfig.java
+++ src/main/java/com/equipe05/plataformaconstrucao/security/WebSecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableMethodSecurity(
@@ -73,5 +75,15 @@ public class WebSecurityConfig {
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
